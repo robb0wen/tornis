23cda268a5f1278ae69f1964409eb13eca043fe2
Robb Owen <hello@robbowen.digital>
2019-05-20 11:02:01 +0100

    Update to Babel 7

--- rollup.js
+++ rollup.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const rollup = require('rollup');
 const uglify = require('rollup-plugin-uglify-es');
+const babel = require('rollup-plugin-babel');
+
+const useES5 = process.argv.includes('--es5');
+const plugins = useES5 ? [babel(), uglify()] : [uglify()];
+const filename = useES5 ? 'tornis.es5.js' : 'tornis.js';
 
 async function build() {
     const bundle = await rollup.rollup({
         input: path.join(__dirname,'/src/tornis.js'),
-        plugins: [
-            uglify()
-        ]
+        plugins
     });
 
     await bundle.write({
         format: 'umd',
         name: 'tornis',
-        file: path.join(__dirname, '/dist/tornis.js')
+        file: path.join(__dirname, `/dist/${filename}`)
     });
 };
 
